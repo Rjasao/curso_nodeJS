bd3b05b68ce929e164f6f4a5ec608d1cfa951ca1
Rjasao <rjasao@gmail.com>
2020-09-22 15:51:43 -0300

    nodeJS_Express_Midd_Cookies

--- exp/app.js
+++ exp/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
-const adminRoutes = require ('./routes/admin')
-const usuarioRoutes = require ('./routes/usuarios')
+const adminRoutes = require ('./routes/admin');
+const usuarioRoutes = require ('./routes/usuarios');
+const cookieParser = require('cookie-parser');
 
 app.use(express.json());
-
+app.use(cookieParser());
 app.use((req, res, next)=>{
     console.log('Executando Middleware em nível de aplicação');
     return next();
@@ -14,6 +15,19 @@ app.get('/',(req, res)=>{
    // res.statusCode = 200;
     res.send('Vai Corintians');
 
+})
+
+app.get('/setcookie',(req, res)=>{
+    res.cookie('user','Ricardo T. Bela',{maxAge:900000, httpOnly: true});
+    return res.send('Cookie foi gravado com sucesso!');
+})
+
+
+app.get('/getcookie',(req, res)=>{
+    let user = req.cookies['user'];
+    if(user){
+        return res.send(user);
+    }
 })
 
 app.use('/admin', adminRoutes)
